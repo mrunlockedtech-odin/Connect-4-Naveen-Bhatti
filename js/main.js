16c85d23e662a084e0274ab3e40049d0de828fec
mrunlockedtech <jrbhrn@gmail.com>
2022-09-09 12:17:32 -0500

    Add CERs and Event Listeners to JS files

--- js/main.js
+++ js/main.js
@@ -7,11 +7,23 @@
 
 
 /*------------------------ Cached Element References ------------------------*/
+const resetButton = document.getElementById("reset-button")
+
+const gridEls = document.querySelectorAll(".gridEntry")
+
+const boardEle = document.querySelector("#board")
+
+const outMsg = document.getElementById("msg")
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+resetButton.addEventListener("click", evt => {
+  console.log(evt.target.id)
+})
+boardEle.addEventListener("click", evt => {
+  console.log(evt.target.id)
+})
 
 
 /*-------------------------------- Functions --------------------------------*/
