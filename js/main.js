293f8f5ab58a67cb7dc0d9fd8680b346b48ca29f
mrunlockedtech <jrbhrn@gmail.com>
2022-09-09 13:10:17 -0500

    Add horizontal solutions for winning Array

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 /*-------------------------------- Constants --------------------------------*/
+const winningPlacement = [
+  //Horizontal Solutions
+  [0,1,2,3],[1,2,3,4],[2,3,4,5],[3,4,5,6],
+  [7,8,9,10],[8,9,10,11],[9,10,11,12],[10,11,12,13],
+  [14,15,16,17],[15,16,17,18],[16,17,18,19],[17,18,19,20],
+  [21,22,23,24],[22,23,24,25],[23,24,25,26],[24,25,26,27],
+  [28,29,30,31],[29,30,31,32],[30,31,32,33],[31,32,33,34],
+  [35,36,37,38],[36,37,38,39],[37,38,39,40],[38,39,40,41]
 
+]
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let board, turn, winner, winCount
 
 
 /*------------------------ Cached Element References ------------------------*/
@@ -27,3 +36,20 @@ boardEle.addEventListener("click", evt => {
 
 
 /*-------------------------------- Functions --------------------------------*/
+init()
+
+function init(){
+  board = [
+    null,null,null,null,null,null,null,
+    null,null,null,null,null,null,null,
+    null,null,null,null,null,null,null,
+    null,null,null,null,null,null,null,
+    null,null,null,null,null,null,null,
+    null,null,null,null,null,null,null
+  ]
+  resetButton.setAttribute("hidden",true)
+  turn = 1
+  winner = null
+  resetAnimations()
+  render()
+}
